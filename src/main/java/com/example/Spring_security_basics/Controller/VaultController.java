3c124f2781510ad0252c5cc6a985aa6abb8fb1cb
ppatel-tech <purushottampatel.tech@gmail.com>
2026-03-01 14:53:00 +0530

    starting commit

--- src/main/java/com/example/Spring_security_basics/Controller/VaultController.java
+++ src/main/java/com/example/Spring_security_basics/Controller/VaultController.java
@@ -4,10 +4,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class VaultController {
 
-    @GetMapping
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/public/info")
+    public String publicInfo() {
+        return "This is public info , anyone can see this";
     }
+
+    @GetMapping("/private/vault")
+    public String privateVault() {
+        return "This is the secret vault. You should be logged in to see this!";
+    }
+
 }
