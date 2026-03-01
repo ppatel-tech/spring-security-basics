2d8ed375c83e1b059f709d545539eaa88380158d
ppatel-tech <purushottampatel.tech@gmail.com>
2026-03-01 14:46:43 +0530

    initial commit

---
+++ src/test/java/com/example/Spring_security_basics/SpringSecurityBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_security_basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
